77855f8cf7d0ed7b736ddc23d12c57787381ac05
SOFMAGON <ada_hack545@womenintech.mx>
2024-06-05 11:41:47 -0600

    fix: server settings

--- api/server.js
+++ api/server.js
@@ -1,21 +1,31 @@
+const fs = require("fs")
+const path = require("path")
+const db = JSON.parse(fs.readFileSync(path.join("db.json")))
+
 // Ver https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
+// Usar middlewares predeterminados
 server.use(middlewares);
-// Antes de server.use(router)
+
+// Reescribir rutas antes de usar el router
 server.use(
-	jsonServer.rewriter({
-		"/api/lugares": "/lugares",
-		"/api/lugares/:id": "/lugares/:id"
-	})
+  jsonServer.rewriter({
+    "/api/lugares": "/lugares",
+    "/api/lugares/:id": "/lugares/:id"
+  })
 );
 
+// Usar el router
 server.use(router);
-server.listen(3000, () => {
-	console.log("El servidor JSON está funcionando");
+
+// Usar el puerto proporcionado por el entorno (como Vercel) o 3000 por defecto
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`El servidor JSON está funcionando en el puerto ${port}`);
 });
 
 // Exporta la API del Servidor
